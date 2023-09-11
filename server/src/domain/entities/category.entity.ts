6f7a19c8943e93ecf684b5c72c6345d2ebe4810e
Joaquin Rampa <rampajoa@gmail.com>
2023-09-11 20:21:55 -0300

    category-entity-2

---
+++ server/src/domain/entities/category.entity.ts
@@ -0,0 +1,23 @@
+class Category{
+    private id: string;
+    private name: string;
+    private color: string;
+
+    private constructor(id: string, name: string, color: string){
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+    
+    public getId(): string {
+        return this.id;
+      }
+
+    public getName(): string {
+        return this.name;
+      }
+
+    public getColor(): string {
+        return this.color;
+      }
+}
