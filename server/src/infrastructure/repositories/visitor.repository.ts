d7bffcbdbb3b0f7c77b45ea67796185175a059d3
TobiasActis <tobiasnactis@gmail.com>
2023-09-12 11:54:33 -0300

    add visitor repository

---
+++ server/src/infrastructure/repositories/visitor.repository.ts
@@ -0,0 +1,28 @@
+import Visitor from "../../domain/entities/visitor.entity";
+
+class VisitorRepository {
+
+    private visitors : Visitor[];
+
+    public constructor(){
+        this.visitors = [];
+    }
+
+    public async save(visitor : Visitor): Promise<void>{
+        const saveVisitor = this.visitors.find(a => a.getId() == visitor.getId());
+
+        if (saveVisitor){
+            this.visitors.splice(this.visitors.indexOf(saveVisitor),1);
+        }
+        this.visitors.push(visitor);
+    }
+
+    public async findOneById(id : string): Promise<Visitor | null>{
+        const visitor = this.visitors.find(a => a.getId() == id);
+        return visitor ? visitor : null;
+    }
+
+
+
+}
+
