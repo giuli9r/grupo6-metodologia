0d79198d82b4114b5906243e20bbc1cc2714c0b6
Braian Vega <braianve7@gmail.com>
2023-10-09 20:05:29 -0300

    fix claim handler

--- server/src/application/handlers/create.claim.handler.ts
+++ server/src/application/handlers/create.claim.handler.ts
@@ -1,7 +1,8 @@
 import {VisitorRepository} from '../../infrastructure/repositories/visitor.repository';
-import  {Claim} from '../../domain/entities/claim.entity';
+import Claim from '../../domain/entities/claim.entity';
 import {CreateClaimCommand} from '../commands/claim/create.claim.command';
 import Visitor from '../../domain/entities/visitor.entity';
+import Category from '../../domain/entities/category.entity';
 
 
 class CreateClaimHandler{
@@ -14,38 +15,34 @@ class CreateClaimHandler{
         this.visitorRepository;
     }
 
-    private checkPin(pin: string): boolean {
-        // Verifica que el PIN tenga exactamente 6 dígitos numéricos.
-        return /^\d{6}$/.test(pin);
-      }
+
     
-    private validatePin(enteredPin: string, visitor: Visitor): boolean {
-        // Verifica que el PIN ingresado coincida con el PIN del visitante.
-        //Declarar el PIN en entities 
-        return enteredPin === visitor.pin; 
-    }
     
-    public async execute(visitorPin: string, claimDetails: string): Promise<void> {
-    // Buscar el visitante por su PIN
-    const visitor = this.visitorRepository.findByPin(visitorPin);
+    
+    public async execute(command: CreateClaimCommand): Promise<void> {
+    // Buscar el visitante
+    const visitor = this.visitorRepository.findOneById(command.getOwnerId());
         
     if (!visitor) {
-    throw new Error('Visitante no encontrado o PIN incorrecto.');
+    throw new Error('Visitante no encontrado ');
     }
 
     // Validar el formato del PIN ingresado
-    if (!this.checkPin(visitorPin)) {
-    throw new Error('El PIN debe tener 6 dígitos numéricos.');
+    if (!visitor.isValidPin(command.getPin())) {
+      throw new Error('PIN incorrecto.');
     }
 
-    // Validar que el PIN ingresado coincida con el PIN del visitante
-    if (!this.validatePin(visitorPin, visitor)) {
-    throw new Error('PIN incorrecto.');
-    }
+    const title= command.getTittle();
+    const description=command.getDescription();
+    const category=command.getCategory();
+    const location= command.getLocation();
     
     const claim = Claim.create(
-      visitor,
-      visitorPin,
+        visitor,
+        title,
+        description,
+        category,
+        location,
     );
   }
 }
