53ed1dd87e64981fa7f4dd1b4f2cb1bd986f01de
cassolo <129336588+cassolo@users.noreply.github.com>
2023-10-23 20:03:00 -0300

    fix create visitor command

--- server/src/application/commands/create.visitor.command.ts
+++ server/src/application/commands/create.visitor.command.ts
@@ -1,21 +1,16 @@
 export class CreateVisitorCommand {
-    private readonly id: string ;
     private readonly ip: string ;
     private readonly nickname: string ;
     
     constructor (
-        id: string,
         ip: string,
         nickname : string
         )
         { 
-            this.id = id;
             this.ip = ip;
             this.nickname = nickname;
         }
-        getId(): string{
-            return this.id
-        }
+
         getIp(): string{
             return this.ip
         }
