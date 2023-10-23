26e52c44f2230d1f4e3199e5a238861d855cf13a
cassolo <129336588+cassolo@users.noreply.github.com>
2023-10-23 19:35:14 -0300

    fix create claim command

--- server/src/application/commands/create.claim.command.ts
+++ server/src/application/commands/create.claim.command.ts
@@ -3,17 +3,17 @@ import Claim from "../../domain/entities/claim.entity";
 import Visitor from "../../domain/entities/visitor.entity";
 
 export class CreateClaimCommand {
-    private readonly id: string;
+
     private readonly owner: Visitor;
     private readonly title:string;
     private readonly description:string;
     private readonly category: Category;
     private readonly location: string;
     private readonly createdAt: Date;
-    private readonly cloneOf?: Claim;
+    private readonly cloneOf: Claim | null;
   
-    constructor(id: string, owner: Visitor, title: string, description: string, category: Category, location: string, createdAt: Date, cloneOf?: Claim) {
-      this.id = id;
+    constructor( owner: Visitor, title: string, description: string, category: Category, location: string, createdAt: Date, cloneOf: Claim | null) {
+
       this.owner = owner;
       this.title = title;
       this.description = description;
@@ -22,8 +22,14 @@ export class CreateClaimCommand {
       this.createdAt = createdAt;
       this.cloneOf = cloneOf;
     }
-    getId(): string {
-      return this.id;
+
+
+    getOwnerId(): string {
+      return this.owner.getId();
+    }
+
+    getOwnerPin(): string {
+      return this.owner.getPin();
     }
   
     getOwner(): Visitor {
@@ -50,8 +56,8 @@ export class CreateClaimCommand {
         return this.createdAt;
       }
 
-    getCloneOf(): Claim | undefined {
+    getCloneOf(): Claim | null {
         return this.cloneOf;
     }
   }
-  
+  export default CreateClaimCommand;
