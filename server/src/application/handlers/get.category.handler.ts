6746dbfa33b185b2f6c76191a52173f8cced38f9
cassolo <129336588+cassolo@users.noreply.github.com>
2023-11-05 13:44:23 -0300

    created get category action, edited get category handler

--- server/src/application/handlers/get.category.handler.ts
+++ server/src/application/handlers/get.category.handler.ts
@@ -1,17 +1,31 @@
 import Category from "../../domain/entities/category.entity";
 import categoryRepository, { CategoryRepository }  from "../../infrastructure/repositories/category.repository";
-
+import GetCategoriesCommand from "application/commands/get.categories.command";
 
 
 class GetCategoryHandler{
-  private categoryRespository: CategoryRepository;
+  private categoryRepository: CategoryRepository;
 
   public constructor(categoryRepository: CategoryRepository){
-    this.categoryRespository = categoryRepository;
+    this.categoryRepository = categoryRepository;
   }
 
-  public async execute(): Promise<void> {
+  public async execute(command: GetCategoriesCommand): Promise<Category[]> {
+    const name = command.getName();
+    const color = command.getColor();
 
+    // Fetch all categories
+    const categories = await this.categoryRepository.findAll();
+
+    // Filter categories based on name and color
+    const filteredCategories = categories.filter(category => {
+      return (
+        (name === '' || category.getName() === name) &&
+        (color === '' || category.getColor() === color)
+      );
+    });
+
+    return filteredCategories;
   }
 }
 
---
+++ server/src/http/actions/get.category.action.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import GetCategoriesCommand from 'application/commands/get.categories.command';
+import getCategoryHandler from 'application/handlers/get.category.handler';
+
+class GetCategoryAction {
+
+  public async run(req: Request, res: Response) {
+    const { name, color } = req.body;
+
+    try {
+      const command = new GetCategoriesCommand(name, color);
+
+
+      const categories = await getCategoryHandler.execute(command);
+
+
+      console.log("Retrieved Categories:", categories);
+
+      
+      return res.status(200).json({ categories });
+    } catch (error:any)
+    {
+      const e = error as Error;
+      res.status(400).json(
+      { message: e.message }
+      );
+        
+    }
+  }
+}
+
+export default new GetCategoryAction();
