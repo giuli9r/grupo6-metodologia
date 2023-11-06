b8bb63aa34fddf23ce706069d6dd0fa28d57e521
cassolo <129336588+cassolo@users.noreply.github.com>
2023-11-06 14:36:30 -0300

    fix category routes

--- server/src/http/routes/category.routes.ts
+++ server/src/http/routes/category.routes.ts
@@ -1,17 +1,15 @@
 import { Application } from 'express';
 import CommonRoutes from './common.routes';
-import createCategoryAction from '../actions/category/create.category.action';
 import getCategoryAction from 'http/actions/get.category.action';
-import findCategoryAction from '../actions/category/find.category.action';
+
 
 class CategoryRoutes extends CommonRoutes{
     public constructor(app: Application){
         super(app, 'Category');
     }
     public setUpRoutes(): Application {
-        this.app.post('/category', createCategoryAction.run);
-        this.app.get('/category',findCategoryAction.run);
-        this.app.put('/category',getCategoryAction.run);
+        
+        this.app.get('/category',getCategoryAction.run);
 
         return this.app;
     }
